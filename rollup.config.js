0b52326514cfc857f49aaa2a6dceb90b9047eae4
Daniel Dosen <dgdosen@gmail.com>
2021-04-23 22:31:11 -0500

    wip: config tweaks

--- rollup.config.js
+++ rollup.config.js
@@ -1,23 +1,14 @@
 import typescript from 'rollup-plugin-typescript2';
-import del from 'rollup-plugin-delete';
 import pkg from './package.json';
 
 export default [
   {
     input: 'src/index.ts',
     output: [
-      {
-        file: 'playground/src/component-lib/index.js',
-        format: 'esm',
-        banner: '/* eslint-disable */',
-      },
       { file: pkg.main, format: 'cjs' },
       { file: pkg.module, format: 'esm' },
     ],
-    plugins: [
-      del({ targets: ['dist/*', 'playground/src/component-lib'] }),
-      typescript(),
-    ],
+    plugins: [typescript()],
     external: Object.keys(pkg.peerDependencies || {}),
   },
 ];
