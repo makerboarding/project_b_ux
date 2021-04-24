2f9ad85aa24b524d2cc3999eeccef1fe6d9def3e
Daniel Dosen <dgdosen@gmail.com>
2021-04-23 19:35:55 -0500

    updating playground to use control

--- playground/src/App.tsx
+++ playground/src/App.tsx
@@ -1,5 +1,4 @@
-import React from 'react';
-import { SaddleClothTd } from './component-lib/components/SaddleClothTd';
+import { SaddleClothTd } from './component-lib';
 
 function App() {
   return (
@@ -9,7 +8,67 @@ function App() {
           <tr>
             <td>hello</td>
             <SaddleClothTd number={1} key={123}>
+              1
+            </SaddleClothTd>
+            <SaddleClothTd number={2} key={124}>
+              2
+            </SaddleClothTd>
+            <SaddleClothTd number={3} key={125}>
+              3
+            </SaddleClothTd>
+            <SaddleClothTd number={4} key={126}>
+              4
+            </SaddleClothTd>
+            <SaddleClothTd number={5} key={127}>
+              5
+            </SaddleClothTd>
+            <SaddleClothTd number={6} key={128}>
+              6
+            </SaddleClothTd>
+            <SaddleClothTd number={7} key={129}>
+              7
+            </SaddleClothTd>
+            <SaddleClothTd number={8} key={133}>
+              8
+            </SaddleClothTd>
+            <SaddleClothTd number={9} key={143}>
+              9
+            </SaddleClothTd>
+            <SaddleClothTd number={10} key={153}>
+              10
+            </SaddleClothTd>
+            <SaddleClothTd number={11} key={163}>
+              11
+            </SaddleClothTd>
+            <SaddleClothTd number={12} key={163}>
               12
+            </SaddleClothTd>
+            <SaddleClothTd number={13} key={163}>
+              13
+            </SaddleClothTd>
+            <SaddleClothTd number={14} key={163}>
+              14
+            </SaddleClothTd>
+            <SaddleClothTd number={15} key={163}>
+              15
+            </SaddleClothTd>
+            <SaddleClothTd number={16} key={163}>
+              16
+            </SaddleClothTd>
+            <SaddleClothTd number={17} key={163}>
+              17
+            </SaddleClothTd>
+            <SaddleClothTd number={18} key={163}>
+              18
+            </SaddleClothTd>
+            <SaddleClothTd number={19} key={163}>
+              19
+            </SaddleClothTd>
+            <SaddleClothTd number={20} key={163}>
+              20
+            </SaddleClothTd>
+            <SaddleClothTd number={21} key={163}>
+              21
             </SaddleClothTd>
           </tr>
         </table>
